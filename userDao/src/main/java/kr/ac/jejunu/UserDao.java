ee9df5cca9bc381174e2a5e0f4cb8755becf608b
JinHyeok Kang <jinhyeok.kang@gmail.com>
2016-04-01 16:41:34 +0900

    refactoring

--- userDao/src/main/java/kr/ac/jejunu/UserDao.java
+++ userDao/src/main/java/kr/ac/jejunu/UserDao.java
@@ -8,8 +8,7 @@ import java.sql.*;
  */
 public class UserDao {
     public User get(Long id) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection connection = DriverManager.getConnection("jdbc:mysql://db.skyserv.kr/jejunu?characterEncoding=utf8", "jeju", "jejupw");
+        Connection connection = getConnection();
 
         String sql = "select * from userinfo where id = ?";
 
@@ -32,8 +31,7 @@ public class UserDao {
     }
 
     public Long add(User user) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection connection = DriverManager.getConnection("jdbc:mysql://db.skyserv.kr/jejunu?characterEncoding=utf8", "jeju", "jejupw");
+        Connection connection = getConnection();
 
         String sql = "insert into userinfo (name, password) values (?, ?)";
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
@@ -42,6 +40,15 @@ public class UserDao {
 
         preparedStatement.executeUpdate();
 
+        Long id = getLastInsertId(connection);
+
+        preparedStatement.close();
+        connection.close();
+
+        return id;
+    }
+
+    private Long getLastInsertId(Connection connection) throws SQLException {
         PreparedStatement preparedStatement2 = connection.prepareStatement(
                 "select last_insert_id()");
         ResultSet resultSet = preparedStatement2.executeQuery();
@@ -51,9 +58,12 @@ public class UserDao {
 
         resultSet.close();
         preparedStatement2.close();
-        preparedStatement.close();
-        connection.close();
-
         return id;
+    }
+
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        return DriverManager.getConnection("jdbc:mysql://db.skyserv.kr/jejunu?characterEncoding=utf8",
+                "jeju", "jejupw");
     }
 }
