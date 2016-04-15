006f58c4c4729cf174930ad8c2af47686700968c
JinHyeok Kang <jinhyeok.kang@gmail.com>
2016-04-15 17:58:26 +0900

    extract connection maker values to property

--- userDao/src/main/java/kr/ac/jejunu/SimpleConnectionMaker.java
+++ userDao/src/main/java/kr/ac/jejunu/SimpleConnectionMaker.java
@@ -5,13 +5,33 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class SimpleConnectionMaker implements ConnectionMaker {
+    private String driverClass;
+    private String url;
+    private String username;
+    private String password;
+
     public SimpleConnectionMaker() {
     }
 
     @Override
     public Connection getConnection() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-        return DriverManager.getConnection("jdbc:mysql://db.skyserv.kr/jejunu?characterEncoding=utf8",
-                "jeju", "jejupw");
+        Class.forName(driverClass);
+        return DriverManager.getConnection(url, username, password);
+    }
+
+    public void setDriverClass(String driverClass) {
+        this.driverClass = driverClass;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
