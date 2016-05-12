384d441a46a1aa771c9f8077b35907bd4299c292
JinHyeok Kang <jinhyeok.kang@gmail.com>
2016-05-13 01:42:00 +0900

    init filter

---
+++ hello/src/main/java/kr/ac/jejunu/servlet/HelloFilter.java
@@ -0,0 +1,31 @@
+package kr.ac.jejunu.servlet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * Created by jhkang on 5/13/16.
+ */
+public class HelloFilter implements Filter {
+    private final static Logger logger = LoggerFactory.getLogger(HelloFilter.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        logger.info("**********  init  **********");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        logger.info("**********  filter start  **********");
+        chain.doFilter(request, response);
+        logger.info("**********  filter end  **********");
+    }
+
+    @Override
+    public void destroy() {
+        logger.info("**********  destroy  **********");
+    }
+}
