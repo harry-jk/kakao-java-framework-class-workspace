61be1de1d3a3ea3937a8e241f95a8ac63578497a
JinHyeok Kang <jinhyeok.kang@gmail.com>
2016-05-27 12:03:09 +0900

    SessionAttribute

--- hello/src/main/java/kr/ac/jejunu/spring/HelloController.java
+++ hello/src/main/java/kr/ac/jejunu/spring/HelloController.java
@@ -1,18 +1,25 @@
 package kr.ac.jejunu.spring;
 
+import kr.ac.jejunu.hello.HelloModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.MatrixVariable;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by jhkang on 2016-05-27.
  */
 @Controller
 @RequestMapping("/spring")
+@SessionAttributes("helloModel")
 public class HelloController {
+    private final static Logger logger = LoggerFactory.getLogger(HelloController.class);
+
     @RequestMapping("/hello")
     public void hello(Model model) {
         model.addAttribute("hello world!!");
@@ -34,5 +41,57 @@ public class HelloController {
     public String hello4(@PathVariable String hello, @MatrixVariable String name, Model model) {
         model.addAttribute(hello + " !! " + name);
         return "/spring/hello";
+    }
+
+    @RequestMapping("/helloworld")
+    public String hello5(@RequestParam String hello, @RequestParam String name, Model model) {
+        model.addAttribute(hello + " !! " + name);
+        return "/spring/hello";
+    }
+
+    @RequestMapping("/hellomodel")
+    public String hello5(HelloModel model) {
+        logger.info("********** " + model.getHello() + model.getName() + " **********");
+        return "/spring/hellomodel";
+    }
+
+    @RequestMapping("/hellocookie")
+    public String hello6(HelloModel model, @CookieValue(value = "name", defaultValue = "harry") String name,
+                         HttpServletResponse response) {
+        if("harry".equals(name)) {
+            response.addCookie(new Cookie("name", "henry"));
+        } else {
+            response.addCookie(new Cookie("name", "harry"));
+        }
+        return "/spring/hellomodel";
+    }
+
+    @RequestMapping("/hellosession")
+    public String hello7(HelloModel model, HttpSession session) {
+        String name = (String) session.getAttribute("name");
+        model.setName(name);
+        if("harry".equals(name)) {
+            session.setAttribute("name", "henry");
+        } else {
+            session.setAttribute("name", "harry");
+        }
+        return "/spring/hellomodel";
+    }
+
+    @ModelAttribute("helloModel")
+    public HelloModel model() {
+        return new HelloModel();
+    }
+
+    @RequestMapping("sessionattribute")
+    public String hello8(HelloModel model) {
+        model.setHello("Hello");
+        model.setName("harry");
+        return "/spring/hellomodel";
+    }
+
+    @RequestMapping("sessionattributevalue")
+    public String hello9(HelloModel model) {
+        return "/spring/hellomodel";
     }
 }
